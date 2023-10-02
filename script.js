4ec967e80956ea90d19fae6102c86c6ace7cf79e
Glitch (glitch-hello-website) <none>
2023-10-02 03:21:24 +0000

    🦁💼 Checkpoint ./style.css:24111152/828 ./script.js:24111152/396 ./projects.html:24111152/31 ./index.html:24111152/5 ./about.html:24111152/31 ./contact.html:24111152/31

--- script.js
+++ script.js
@@ -59,19 +59,15 @@ function cursor(e){
 navLinks.forEach(link => {
   link.addEventListener("mouseover", () => {
     mouseCursor.classList.add("link-grow");
-    link.classList.add("hovered-link");
   });
   link.addEventListener("mouseleave", () => {
     mouseCursor.classList.remove("link-grow");
-    link.classList.remove("hovered-link");
   });
 });
 
 logo.addEventListener("mouseover", () => {
   mouseCursor.classList.add("link-grow");
-  logo.classList.add("hovered-link"); //yes i know its not a link
 });
 logo.addEventListener("mouseleave", () => {
   mouseCursor.classList.remove("link-grow");
-  logo.classList.remove("hovered-link");
 });
